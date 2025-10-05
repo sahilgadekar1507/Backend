0d0c7b915b9e62edf0d58b34ade0f46f16960308
sahilgadekar1507 <sahilgadekar15@gmail.com>
2025-10-05 10:46:03 +0530

    created all the data models

--- models/todos/sub_todo.models.js
+++ models/todos/sub_todo.models.js
@@ -1,5 +1,18 @@
 import mongoose from "mongoose"
 
-const todoSchema = new mongoose.Schema({}, {timestamps: true})
+const todoSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true,
+    },
+    completed: {
+        type: Boolean,
+        default: false,
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    }
+}, {timestamps: true})
 
 export const SubTodo = mongoose.model('SubTodo', todoSchema)
